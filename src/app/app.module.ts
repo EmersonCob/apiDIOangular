10382166c71a65f471d7cd5c933777157d677e45
EmersonCob <58399211+EmersonCob@users.noreply.github.com>
2021-05-12 20:32:51 -0300

    Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeModule } from './pages/home/home.module';
 import { BookmarksModule } from './pages/bookmarks/bookmarks.module';
-import { StoreModule } from '@ngrx/store';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -17,6 +20,7 @@ import { StoreModule } from '@ngrx/store';
     HomeModule,
     BookmarksModule,
     StoreModule.forRoot({}),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -2,8 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 
 
-
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
