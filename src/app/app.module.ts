449f6059bad3cb99dfbdc7761abc287e849a77f5
EmersonCob <58399211+EmersonCob@users.noreply.github.com>
2021-05-12 19:45:41 -0300

    Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeModule } from './pages/home/home.module';
 import { BookmarksModule } from './pages/bookmarks/bookmarks.module';
+import { StoreModule } from '@ngrx/store';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { BookmarksModule } from './pages/bookmarks/bookmarks.module';
     AppRoutingModule,
     HomeModule,
     BookmarksModule,
+    StoreModule.forRoot({}),
   ],
   providers: [],
   bootstrap: [AppComponent]
